0d9aea33a77f07ea2cde599cb660011f22b8bdb0
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-28 13:18:03 -0400

    space after 3 digit

--- pinCodeValidation.sh
+++ pinCodeValidation.sh
@@ -2,7 +2,7 @@
 printf ".........WELCOME TO PIN CODE VALIDATION PROGRAM.........\n"
 
 shopt -s extglob
-pattern="[0-9]{6}"
+pattern="^[0-9]{3}(\s)*[0-9]{3}$"
 read -p "Enter pin: " pin
 if [[ $pin =~ $pattern ]]
 then
